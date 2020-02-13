5247c887142401880b1872a2db96a22fc298f3a3
camvaz <camposvazquezv@gmail.com>
2020-02-13 12:17:02 -0600

    gitignore and test number 5

--- parcial-1/strats.py
+++ parcial-1/strats.py
@@ -1,15 +1,79 @@
 import datetime
 
 class Strats:
-    
     def __init__(self):
         self.date = datetime.date.today()
-        self.name = f"\n\t#  Codigo de metodos de prueba para el primer parcial de Control y Calidad de Software\n\n Fecha: {self.date} \tNombre: Victor Manuel Campos Vazquez"
+        self.name = f"\n\t#  Codigo de metodos de prueba para el primer parcial de Control y Calidad de Software\n\n'\
+                     + Fecha: {self.date} \t Nombre: Victor Manuel Campos Vazquez"
 
     def prueba_1(self, stringToTest: str):
         return stringToTest.split(', ')
+    
+    def prueba_2(self, stringToTest: str):
+        return len(stringToTest.split(', ')) / 2
         
+    def prueba_3(self, stringToTest: str):
+        return len(stringToTest.split(', ')) / 2
 
-strat = Strats()
+    def prueba_4(self, stringToTest: str):
+        return len(stringToTest.split(', ')) / 2
 
-print(strat.prueba_1("Queen, 9, Metallica, 7, Doors, 8, Nirvana, 6"))
+    def prueba_5(self, stringToTest: str):
+        values = stringToTest.split(', ')
+        numbers = []
+
+        # Obtenemos datos int:
+        for i in values:
+            try:
+                numbers.append(int(i))
+            except ValueError:
+                pass
+
+        sortedList = numbers[:]
+        sortedList.sort(reverse=True)
+        
+        # Comparamos original con ordenada
+        return sortedList == numbers
+
+    
+
+                              
+                              
+                              
+
+    #=== PLAYGROUND
+
+    # def prueba_3(self, stringToTest: str):
+    #     return
+
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+    # def prueba_3(self, stringToTest: str):
+    #     return
+
+# strat = Strats()
+# print(strat.prueba_5('Queen, 10, Metallica, 8, Doors, 8, Nirvana, 7, Guns and Roses, 6, Caifanes, 5'))
--- parcial-1/test.py
+++ parcial-1/test.py
@@ -4,10 +4,30 @@ from strats import Strats
 class StratsTests(unittest.TestCase):
     def setUp(self):
         self.strat = Strats()
-    
+        self.minValues = 5
+        self.maxValues = 10
+
     def testCommaSeparatedValue(self):
         self.assertEqual(['Queen', '9', 'Metallica', '7', 'Doors', '8', 'Nirvana', '6'],
                          self.strat.prueba_1("Queen, 9, Metallica, 7, Doors, 8, Nirvana, 6"))
 
+    def testLessThan5(self):
+        self.assertFalse(self.strat.prueba_2('Queen, 9, Metallica, 7') >= self.minValues)
+        
+    def testLessThan10MoreThan5(self):
+        self.assertTrue(self.strat.prueba_2('Queen, 10, Metallica, 8, Doors, 8, Nirvana, 7, Guns and Roses, 6, Caifanes, 5') <= self.maxValues and\
+             self.strat.prueba_2('Queen, 10, Metallica, 8, Doors, 8, Nirvana, 7, Guns and Roses, 6, Caifanes, 5') >= self.minValues)
+    
+    def testLessThan10(self):
+        self.assertFalse(self.strat.prueba_4('Queen, 10, Metallica, 8, Doors, 8, Nirvana, 7, Guns and Roses, 6, Caifanes, 5, '
+                                            +'Molotov, 4, Kanye, 3, Tyler, 2, Rocky, 1, Joey, 0') <=10)
+    
+    def testOrderedNumbers(self):
+        self.assertTrue(self.strat.prueba_5('Queen, 10, Metallica, 8, Doors, 8, Nirvana, 7, Guns and Roses, 6, Caifanes, 5'))
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
+    
