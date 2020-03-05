a2fe7f1d12981677d5fa181ac29995721184d19e
camvaz <camposvazquezv@gmail.com>
2020-03-05 00:11:30 -0600

    mean test

--- PSP-0/test.py
+++ PSP-0/test.py
@@ -7,15 +7,21 @@ class StatsTests(unittest.TestCase):
         self.statsB = Stats([186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601])
         self.statsC = Stats([15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2])
     
+    # Prueba 1 
     def testASet(self):
         self.assertEqual(self.statsA.standardDeviation(),572.03)
 
+    # Prueba 2
     def testBSet(self):
         self.assertEqual(self.statsB.standardDeviation(),625.63)
 
+    # Prueba 3
     def testCSet(self):
         self.assertEqual(self.statsC.standardDeviation(),62.26)
 
+    # Prueba de promedio 
+    def testavg(self):
+        self.assertEqual(Stats([1,2,3]).mean(),2)
 
 if __name__ == '__main__':
     unittest.main()
